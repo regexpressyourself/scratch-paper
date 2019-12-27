3b9ee779e711516e22260e7b0929260ccb335d18
Sam <samuel.messina@siu.edu>
2019-12-26 18:20:50 -0600

    add static path

--- config/app_routes.js
+++ config/app_routes.js
@@ -10,6 +10,7 @@ module.exports = (app) => {
   app.use('/css', express.static('public/static/css'));
   app.use('/js', express.static('public/static/js'));
   app.use('/fonts', express.static('public/static/fonts'));
+  app.use('/static', express.static('public/static'));
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/static/dist/index.html'));
